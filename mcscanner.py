87b501deba27ae41f4538e29af1c852c8e96cb3d
octavio <octavio.delser@gmail.com>
2022-09-12 16:19:40 +0100

    improvements general

--- mcscanner.py
+++ mcscanner.py
@@ -1,87 +1,75 @@
 from mcstatus import JavaServer
+from mcstatus.server import PingResponse
 import os
-import math
 import threading
 import time
 import argparse
+from multiprocessing import Pool
+from typing import List, Union, Tuple
+try:
+    import tqdm
+except ImportError:
+    print("Please install tqdm (pip) for progress bar")
 
 parser = argparse.ArgumentParser(description='get files for procesing')
 parser.add_argument("-i", "--inputfile", type=str, help="put in the file with all the server IP's")
-parser.add_argument("-o","--outputfile", type=str, help="the name of the file to put in the results")
-parser.add_argument("-p","--publicserverlist", type=str, help="put in the file with the public server list (public.txt)")
-parser.add_argument("-v","--version", type=str, default="", required=False, help="you can specify the minecarft server you wanna find")
+parser.add_argument("-t", "--threads", type=int, help="number of threads default=20", default=20)
+parser.add_argument("-o", "--outputfile", type=str, help="the name of the file to put in the results")
+parser.add_argument("-p", "--publicserverlist", type=str,
+                    help="put in the file with the public server list (public.txt)")
+parser.add_argument("-v", "--version", type=str, default="", required=False,
+                    help="you can specify the minecarft server you wanna find")
+parser.add_argument("-min", "--minplayers", type=int, default=0, required=False,
+                    help="you can specify the minecarft server you wanna find")
+parser.add_argument("-d", "--debug", type=bool, default=False, required=False,
+                    help="If specified, each request will be printed with response and reasxon")
 args = parser.parse_args()
 
-masscan = []
 print('Multithreaded mass minecraft server status checker by Footsiefat/Deathmonger')
 
 time.sleep(1)
 
 inputfile = args.inputfile
-outputfile = args.outputfile
-publicserverlist = args.publicserverlist
+f = open(args.publicserverlist, 'r')
+public_server_ips = set(f.read().split(" "))
+f.close()
+outfile = open(args.outputfile, 'a+')
+existing_server_ips = set(outfile.read().split(" "))
+outfile.close()
 searchterm = args.version
-
-outfile = open(outputfile, 'a+')
-outfile.close
-
-fileHandler = open (inputfile, "r")
-listOfLines = fileHandler.readlines()
+thread_count = args.threads
+minplayers = args.minplayers
+fileHandler = open(inputfile, "r")
+masscan_output = fileHandler.readlines()
 fileHandler.close()
-
-for line in listOfLines:
-    if line.strip()[0] != "#":
-        masscan.append(line.strip().split(' ',4)[3])
-
-
-
-def split_array(L,n):
-    return [L[i::n] for i in range(n)]
-
-
-threads = int(input('How many threads do you want to use? (Recommended 20): '))
-
-time.sleep(2)
-
-if len(masscan) < int(threads):
-    threads = len(masscan)
-
-
-split = list(split_array(masscan, threads))
-
+server_ips = [line.strip().split(' ', 4)[3] for line in masscan_output if line.strip()[0] != "#"]
 exitFlag = 0
 
-class myThread (threading.Thread):
-    def __init__(self, threadID, name):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-    def run(self):
-        print ("Starting Thread " + self.name)
-        print_time(self.name)
-        print ("Exiting Thread " + self.name)
+content_out = []
 
-def print_time(threadName):
-    for z in split[int(threadName)]:
-        if exitFlag:
-            threadName.exit()
-        try:
-            ip = z
-            server = JavaServer(ip,25565)
-            status = server.status()
-        except:
-            print("Failed to get status of: " + ip)
-        else:
-            print("Found server: " + ip + " " + status.version.name + " " + str(status.players.online))
-            if searchterm in status.version.name:
-                with open(outputfile) as f:
-                    if ip not in f.read():
-                        with open(publicserverlist) as g:
-                            if ip not in g.read():
-                                text_file = open(outputfile, "a")
-                                text_file.write(ip + " " + status.version.name.replace(" ", "_") + " " + str(status.players.online))
-                                text_file.write(os.linesep)
-                                text_file.close()
+total_servers= len(server_ips)
 
-for x in range(threads):
-    thread = myThread(x, str(x)).start()
+def ping_server(ip) -> Tuple[bool, Union[str, PingResponse]]:
+    try:
+        # raise Exception
+        return True, JavaServer(ip, 25565).status()
+    except Exception as e:
+        return False, f"[ FAIL ] IP: {ip} failed with exception: {e}"
+
+
+def log_data(ip):
+    success, resp = ping_server(ip)
+    if success and resp.players.online > minplayers:
+        log = f"{ip} {resp.version.name.replace(' ', '_')} {resp.players.online}"
+        content_out.append(log)
+        print(f'[ SUCC ] {log}')
+    elif args.debug:
+        print(resp)
+
+
+with Pool(thread_count) as p:
+    for _ in tqdm.tqdm(p.imap_unordered(log_data, server_ips), total=len(server_ips)):
+        pass
+outfile = open(args.outputfile, 'a+')
+outfile.writelines(content_out)
+outfile.close()
