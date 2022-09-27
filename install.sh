2ee947b4bf3c3f302de69b1342bf3b882cabb6c5
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-09-27 20:45:19 +0200

    Added distro specific installer

--- install.sh
+++ install.sh
@@ -3,8 +3,22 @@ mkdir /usr/local/bin
 # Copy python file into /usr/bin
 cp ./mcscanner.py /usr/local/bin/mcscanner
 
-# Install masscan
-sudo apt install masscan
+# get the disctro specific package manager
+declare -A osInfo;
+osInfo[/etc/redhat-release]=yum install
+osInfo[/etc/arch-release]=pacman -S
+osInfo[/etc/gentoo-release]=emerge --ask --verbose
+osInfo[/etc/SuSE-release]=zypper install
+osInfo[/etc/debian_version]=apt-get install
+osInfo[/etc/alpine-release]=apk add
+ 
+# I stall masscan
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        sudo ${osInfo[$f]} masscan python pip
+    fi
+done
 
 cd /usr/local/bin/ || exit
 
@@ -12,6 +26,6 @@ cd /usr/local/bin/ || exit
 wget https://raw.githubusercontent.com/robertdavidgraham/masscan/master/data/exclude.conf
 
 # Install mcstatus from pip
-pip3 install mcstatus
+pip install mcstatus
 
 echo "Successfully installed mc-scanner!"
