ddafd1419507854981c42a9dd534f4dd524d0e2d
kyrill belomenov <kyrill.belomenov@outlook.de>
2023-03-15 11:29:31 +0100

    init commit

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
 # Minecraft-Server-Scanner
+
 Let's you scan the entire internet in a couple of hours and identify all Minecraft servers on IPV4
 
-
 **Installation:**
+
 ```
 sudo apt install masscan (Or distro specific installer command)
 wget https://raw.githubusercontent.com/robertdavidgraham/masscan/master/data/exclude.conf
 pip3 install mcstatus
+pip3 install dhook
 ```
 
 **start scanning for servers:**
+
 ```
 sudo masscan -p25565 0.0.0.0/0 --max-rate <maxrate> --excludefile exclude.conf -oL masscan.txt (Set maxrate to a limit that you are comfortable with and won't melt your router)
 ```
 
 **find real minecraft servers from the results of the scan:**
+
 ```
 python mcscanner.py -i masscan.txt -o result.txt -p public.txt
 ```
@@ -22,6 +26,8 @@ python mcscanner.py -i masscan.txt -o result.txt -p public.txt
 ###### for more infomation you can always get help by typing ```python mcscanner.py -h```
 
 - For filtering out public servers you can use my public.txt file but it will probably be a bit outdated by now
-- Hit enter and wait for my program to check and grab the metadata for all the servers found, once this is done you will have a massive amount of servers (A lot of them being private ones) to have some fun with. The output file includes the IP Address, Version and Current Players online of found servers.
+- Hit enter and wait for my program to check and grab the metadata for all the servers found, once this is done you will
+  have a massive amount of servers (A lot of them being private ones) to have some fun with. The output file includes
+  the IP Address, Version and Current Players online of found servers.
 
 **DISCLAIMER: THIS IS FOR EDUCATIONAL PURPOSES ONLY AND I AM NOT RESPONSIBLE FOR ANY MISUSE OF THESE TOOLS!**
--- mcscanner.py
+++ mcscanner.py
@@ -1,15 +1,16 @@
 from mcstatus import JavaServer
 import os
-import math
 import threading
 import time
 import argparse
 
 parser = argparse.ArgumentParser(description='get files for procesing')
 parser.add_argument("-i", "--inputfile", type=str, help="put in the file with all the server IP's")
-parser.add_argument("-o","--outputfile", type=str, help="the name of the file to put in the results")
-parser.add_argument("-p","--publicserverlist", type=str, help="put in the file with the public server list (public.txt)")
-parser.add_argument("-v","--version", type=str, default="", required=False, help="you can specify the minecarft server you wanna find")
+parser.add_argument("-o", "--outputfile", type=str, help="the name of the file to put in the results")
+parser.add_argument("-p", "--publicserverlist", type=str,
+                    help="put in the file with the public server list (public.txt)")
+parser.add_argument("-v", "--version", type=str, default="", required=False,
+                    help="you can specify the minecarft server you wanna find")
 args = parser.parse_args()
 
 masscan = []
@@ -25,17 +26,16 @@ searchterm = args.version
 outfile = open(outputfile, 'a+')
 outfile.close
 
-fileHandler = open (inputfile, "r")
+fileHandler = open(inputfile, "r")
 listOfLines = fileHandler.readlines()
 fileHandler.close()
 
 for line in listOfLines:
     if line.strip()[0] != "#":
-        masscan.append(line.strip().split(' ',4)[3])
+        masscan.append(line.strip().split(' ', 4)[3])
 
 
-
-def split_array(L,n):
+def split_array(L, n):
     return [L[i::n] for i in range(n)]
 
 
@@ -46,20 +46,22 @@ time.sleep(2)
 if len(masscan) < int(threads):
     threads = len(masscan)
 
-
 split = list(split_array(masscan, threads))
 
 exitFlag = 0
 
-class myThread (threading.Thread):
+
+class myThread(threading.Thread):
     def __init__(self, threadID, name):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
+
     def run(self):
-        print ("Starting Thread " + self.name)
+        print("Starting Thread " + self.name)
         print_time(self.name)
-        print ("Exiting Thread " + self.name)
+        print("Exiting Thread " + self.name)
+
 
 def print_time(threadName):
     for z in split[int(threadName)]:
@@ -67,7 +69,7 @@ def print_time(threadName):
             threadName.exit()
         try:
             ip = z
-            server = JavaServer(ip,25565)
+            server = JavaServer(ip, 25565)
             status = server.status()
         except:
             print("Failed to get status of: " + ip)
@@ -79,9 +81,11 @@ def print_time(threadName):
                         with open(publicserverlist) as g:
                             if ip not in g.read():
                                 text_file = open(outputfile, "a")
-                                text_file.write(ip + " " + status.version.name.replace(" ", "_") + " " + str(status.players.online))
+                                text_file.write(
+                                    ip + " " + status.version.name.replace(" ", "_") + " " + str(status.players.online))
                                 text_file.write(os.linesep)
                                 text_file.close()
+
 
 for x in range(threads):
     thread = myThread(x, str(x)).start()
